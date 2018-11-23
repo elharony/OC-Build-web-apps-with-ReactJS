22a2930be8345e80896992e8a290a77680838e4e
elharony <p.yahyaelharony@gmail.com>
2018-11-23 17:23:54 +0200

    Sort the products [Static]

--- src/Components/ProductList.js
+++ src/Components/ProductList.js
@@ -12,17 +12,39 @@ class ProductList extends React.Component {
                 direction: 'desc'
             }
         }
+
+        // Binding
+        this.sortByColumnAndDirection = this.sortByColumnAndDirection.bind(this)
+    }
+
+    // Sort by 'column' and 'direction'
+    sortByColumnAndDirection(objA, objB) {
+        let isDesc = this.state.sort.direction === 'desc' ? 1 : -1
+        let [a, b] = [objA[this.state.sort.column], objB[this.state.sort.column]]
+        if(this.state.sort.column === 'price') {
+            [a, b] = [a, b].map((value) => parseFloat(value.replace(/[^\d.]/g, ''), ''), 10)
+        }
+        if(a > b) {
+            return 1 * isDesc
+        }
+        if(a < b) {
+            return -1 * isDesc
+        }
+        return
+    }
+
+    sortProducts() {
+        // Convert 'obj' to 'array'
+        let productsAsArray = Object.keys(this.props.products).map((productId) => this.props.products[productId])
+        // Get the sorted array
+        return productsAsArray.sort(this.sortByColumnAndDirection)
     }
 
 
     render() {
-
-        // Convert 'obj' to 'array'
-        let ProductsAsArray = Object.keys(this.props.products).map((productId) => this.props.products[productId])
-        
-        // Insert each product into a row
-        let rows = [];
-        ProductsAsArray.forEach((product) => {
+        // Loop over the sorted array
+        let rows = []
+        this.sortProducts().forEach((product) => {
 
             // Display certain products based on the filters
             if(product.name.indexOf(this.props.searchQuery) === -1 || (!product.stocked && this.props.inStock)) {
